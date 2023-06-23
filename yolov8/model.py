3c2fa37d5e7193a5e519484e15140acedd7c714d
Hardik Dava <39372750+hardikdava@users.noreply.github.com>
2023-06-23 09:18:18 +0200

    Create model.py

---
+++ yolov8/model.py
@@ -0,0 +1,148 @@
+from label_studio_ml.model import LabelStudioMLBase
+import os
+from ultralytics import YOLO
+from PIL import Image
+from label_studio_ml.utils import get_image_size, \
+    get_single_tag_keys
+
+
+category_map = {
+    1: 'person',
+    2: 'bicycle',
+    3: 'car',
+    4: 'motorcycle',
+    5: 'airplane',
+    6: 'bus',
+    7: 'train',
+    8: 'truck',
+    9: 'boat',
+    10: 'traffic light',
+    11: 'fire hydrant',
+    13: 'stop sign',
+    14: 'parking meter',
+    15: 'bench',
+    16: 'bird',
+    17: 'cat',
+    18: 'dog',
+    19: 'horse',
+    20: 'sheep',
+    21: 'cow',
+    22: 'elephant',
+    23: 'bear',
+    24: 'zebra',
+    25: 'giraffe',
+    27: 'backpack',
+    28: 'umbrella',
+    31: 'handbag',
+    32: 'tie',
+    33: 'suitcase',
+    34: 'frisbee',
+    35: 'skis',
+    36: 'snowboard',
+    37: 'sports ball',
+    38: 'kite',
+    39: 'baseball bat',
+    40: 'baseball glove',
+    41: 'skateboard',
+    42: 'surfboard',
+    43: 'tennis racket',
+    44: 'bottle',
+    46: 'wine glass',
+    47: 'cup',
+    48: 'fork',
+    49: 'knife',
+    50: 'spoon',
+    51: 'bowl',
+    52: 'banana',
+    53: 'apple',
+    54: 'sandwich',
+    55: 'orange',
+    56: 'broccoli',
+    57: 'carrot',
+    58: 'hot dog',
+    59: 'pizza',
+    60: 'donut',
+    61: 'cake',
+    62: 'chair',
+    63: 'couch',
+    64: 'potted plant',
+    65: 'bed',
+    67: 'dining table',
+    70: 'toilet',
+    72: 'tv',
+    73: 'laptop',
+    74: 'mouse',
+    75: 'remote',
+    76: 'keyboard',
+    77: 'cell phone',
+    78: 'microwave',
+    79: 'oven',
+    80: 'toaster',
+    81: 'sink',
+    82: 'refrigerator',
+    84: 'book',
+    85: 'clock',
+    86: 'vase',
+    87: 'scissors',
+    88: 'teddy bear',
+    89: 'hair drier',
+    90: 'toothbrush'
+}
+
+class YOLOv8Model(LabelStudioMLBase):
+    def __init__(self, **kwargs):
+        # Call base class constructor
+        super(YOLOv8Model, self).__init__(**kwargs)
+
+        from_name, schema = list(self.parsed_label_config.items())[0]
+        self.from_name = from_name
+        self.to_name = schema['to_name'][0]
+        self.labels = []
+        for key, val in category_map.items():
+            self.labels.append(val)
+        self.model = YOLO("yolov8s.pt")
+
+    def predict(self, tasks, **kwargs):
+        """ This is where inference happens: model returns
+            the list of predictions based on input list of tasks
+        """
+        task = tasks[0]
+
+        predictions = []
+        score = 0
+
+        img_path = task["data"]["image"]
+        image_path = self.get_local_path(img_path)
+        img = Image.open(image_path)
+        original_width, original_height = get_image_size(image_path)
+
+        results = self.model.predict(img)
+
+        for result in results:
+            for i, prediction in enumerate(result.boxes):
+                xyxy = prediction.xyxy[0].tolist()
+                predictions.append({
+                    "id": str(i),
+                    "from_name": self.from_name,
+                    "to_name": self.to_name,
+                    "type": "rectanglelabels",
+                    "score": prediction.conf.item(),
+                    "original_width": original_width,
+                    "original_height": original_height,
+                    "image_rotation": 0,
+                    "value": {
+                        "rotation": 0,
+                        "x": xyxy[0] / original_width * 100,
+                        "y": xyxy[1] / original_height * 100,
+                        "width": (xyxy[2] - xyxy[0]) / original_width * 100,
+                        "height": (xyxy[3] - xyxy[1]) / original_height * 100,
+                        "rectanglelabels": [category_map[int(prediction.cls.item())]]
+                    }
+                })
+                score += prediction.conf.item()
+
+        return [{
+            "result": predictions,
+            "score": score / (i + 1),
+            "model_version": "v8s",  # all predictions will be differentiated by model version
+        }]
